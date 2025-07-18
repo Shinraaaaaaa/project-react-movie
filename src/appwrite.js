7d8246fc453fd26b418bd78eb619a197abd7d10e
Steven Hwang <venmails32@gmail.com>
2025-07-18 12:29:56 +0700

    Project done

--- src/appwrite.js
+++ src/appwrite.js
@@ -5,7 +5,7 @@ const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setEndpoint("https://nyc.cloud.appwrite.io/v1")
   .setProject(PROJECT_ID);
 
 const database = new Databases(client);
@@ -30,9 +30,22 @@ export const updateSearchCount = async (searchTerm, movie) => {
         searchTerm,
         count: 1,
         movie_id: movie.id,
-        poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
       });
     }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTrendingMovies = async () => {
+  try{
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+    
+    return result.documents;
   } catch (error) {
     console.error(error);
   }
